ad52a4d80f22087a22626d48cbbfae28b7a55a35
Yi <yf129@duke.edu>
2021-04-26 23:28:11 -0400

    update CA plot

--- 10_code/plotting.py
+++ 10_code/plotting.py
@@ -1,6 +1,5 @@
 ##################################################
 # Contains functions for diff-in-diff plot 
-# and pre-post plot.
 ##################################################
 import pandas as pd
 import numpy as np
@@ -9,43 +8,70 @@ import os
 import sys
 from plotnine import *
 
-sf_df = pd.read_csv("../20_intermediate_files/SF_data.csv", index_col = 0)
-la_df = pd.read_csv("../20_intermediate_files/LA_data.csv", index_col = 0)
-sf_df["standardized_date"] = sf_df["standardized_date"].str.replace(' days','', regex=True).astype(int)
-la_df["standardized_date"] = la_df["standardized_date"].str.replace(' days','', regex=True).astype(int)
+ca_df = pd.read_csv("../20_intermediate_files/CA_state_level.csv")
+ca_df["date"] = pd.to_datetime(ca_df["date"])
+control_df = pd.read_csv("../20_intermediate_files/CA_controls.csv")
+control_df["date"] = pd.to_datetime(control_df["date"])
+ca_df["state"] = "CA"
+control_df["state"] = "control"
+# sf_df["standardized_date"] = sf_df["standardized_date"].str.replace(' days','', regex=True).astype(int)
+# la_df["standardized_date"] = la_df["standardized_date"].str.replace(' days','', regex=True).astype(int)
 
 
 # # Diff in Diff Plot
+# def diff_in_diff_plot(df_treatment, df_control):
+    
+#     #treatment_state = str(list(df_treatment.state.unique())[0])
+      #lower_lim = df_treatment['standardized_date'].min()
+#     upper_lim = df_treatment['standardized_date'].max()+1
+    
+#     plot = (
+#     ggplot() +
+#     geom_smooth(df_treatment[df_treatment['standardized_date'] < 0], 
+#                 aes(x='standardized_date', y='cases', color='county')) 
+#         + 
+#     geom_smooth(df_treatment[df_treatment['standardized_date'] >= 0], 
+#                 aes(x='standardized_date', y='cases', color='county')) + 
+        
+#     geom_smooth(df_control[df_control['standardized_date'] < 0], 
+#                 aes(x='standardized_date', y='cases', color='county')) + 
+#     geom_smooth(df_control[df_control['standardized_date'] >= 0], 
+#                 aes(x='standardized_date', y='cases', color='county'))
+#     + geom_vline(xintercept = 0) 
+#     + xlab('Days before/after Policy Implementation: ' + '. \nRepresented as "0" on the x-axis.') 
+#     + ylab(' \n number of Covid cases per 10,000')
+#     + scale_x_continuous(breaks=range(lower_lim,upper_lim,1))
+#     + theme(figure_size=(12, 6))
+#     + labs(title=str("Difference in Difference Plot - "+treatment_state))
+        
+#     )
+#     return plot
+
 def diff_in_diff_plot(df_treatment, df_control):
-    
-    treatment_state = str(list(df_treatment.state.unique())[0])
-    
- 
-    lower_lim = df_treatment['standardized_date'].min()
-    upper_lim = df_treatment['standardized_date'].max()+1
-    
+    treatment = "red"
+    control = "black"
     plot = (
     ggplot() +
-    geom_smooth(df_treatment[df_treatment['standardized_date'] < 0], 
-                aes(x='standardized_date', y='cases', color='county')) 
+    geom_smooth(df_treatment[df_treatment["date"] < '2020-03-19'], 
+                aes(x='date', y='cases', color = 'state')) 
         + 
-    geom_smooth(df_treatment[df_treatment['standardized_date'] >= 0], 
-                aes(x='standardized_date', y='cases', color='county')) + 
-        
-    geom_smooth(df_control[df_control['standardized_date'] < 0], 
-                aes(x='standardized_date', y='cases', color='county')) + 
-    geom_smooth(df_control[df_control['standardized_date'] >= 0], 
-                aes(x='standardized_date', y='cases', color='county'))
-    + geom_vline(xintercept = 0) 
+     geom_smooth(df_treatment[df_treatment["date"] >= '2020-03-19'], 
+                aes(x='date', y='cases', color = 'state'))  + 
+    geom_smooth(df_control[df_control["date"] < '2020-03-19'], 
+                aes(x='date', y='cases', color = 'state')) + 
+    geom_smooth(df_control[df_control["date"] >= '2020-03-19'], 
+                aes(x='date', y='cases', color = 'state'))
+    + geom_vline(xintercept = '2020-03-19') 
     + xlab('Days before/after Policy Implementation: ' + '. \nRepresented as "0" on the x-axis.') 
     + ylab(' \n number of Covid cases per 10,000')
-    + scale_x_continuous(breaks=range(lower_lim,upper_lim,1))
+    #+ scale_x_continuous(breaks=range(lower_lim,upper_lim,1))
     + theme(figure_size=(12, 6))
-    + labs(title=str("Difference in Difference Plot - "+treatment_state))
+    + labs(title=str("Difference in Difference Plot - "))
         
     )
     return plot
 
 
-graph = diff_in_diff_plot(sf_df, la_df)
-graph.save('CA_diff.pdf', height=6, width=12)
+
+graph = diff_in_diff_plot(ca_df, control_df)
+graph.save('./20_intermediate_file/CA_diff.pdf', height=6, width=12)
